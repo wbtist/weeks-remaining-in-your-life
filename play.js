06494084118ae7e723944bb91fc54f4c712e3432
Istvan Toth <wbtist@gmail.com>
2022-10-07 19:19:38 +0100

    node_modules removed, Server developing

--- play.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-// let parsedData = JSON.parse(data);
-// console.log(parsedData);
-
-
-fetch("./data/male_data.json")
-  .then(response => response.json())
-  .then(json => a = json[1][0]);
--- server.js
+++ server.js
@@ -1,14 +1,56 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
-const app = express();
+const { differenceInDays } = require("date-fns");
 
 const port = 3000;
+const app = express();
+
+// TODO: Variables can be defined and used per page, clean it up!
+let firstname = "";
+let dob = "";
+let birthYear = "";
+let birthMonth = "";
+let birthDay = "";
+const today = new Date();
+let date =
+  today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+let currentYear = today.getFullYear();
+let currentMonth = today.getMonth() + 1;
+let currentDay = today.getDay();
+// let weeksLived;
+// let daysLived;
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("index");
+});
+app.get("/second", (req, res) => {
+  const daysLived = differenceInDays(
+    new Date(currentYear, currentMonth, currentDay),
+    new Date(birthYear, birthMonth, birthDay)
+  );
+  const weeksLived = Math.floor((daysLived + 1) / 7);
+  console.log(dob, daysLived);
+  res.render("second", {
+    firstName: firstname,
+    dob: dob,
+    daysLived: daysLived,
+    weeksLived: weeksLived,
+  });
+});
+
+app.post("/", (req, res) => {
+  const now = new Date();
+  firstname = req.body.firstname;
+  dob = req.body.dob;
+  birthYear = Number(dob.slice(0, 4));
+  birthMonth = Number(dob.slice(5, 7));
+  birthDay = Number(dob.slice(-2));
+  res.redirect("/second");
 });
 
 app.listen(process.env.port || port, () => {
